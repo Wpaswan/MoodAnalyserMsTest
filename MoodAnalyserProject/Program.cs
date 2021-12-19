e1db8534afad9abeaa862292591def93d313cbef
Wpaswan <dablumcapaswan@gmail.com>
2021-12-19 09:07:32 -0800

    [I added a code to analyse the mood happy or sad]

---
+++ MoodAnalyserProject/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Mood
+{
+    static void Main(string[] args)
+    {
+
+    }
+
+    public string AnalyzeMood(String Message)
+    {
+        string mood;
+        if (Message.Contains("Sad"))
+            return mood = "Sad";
+        else
+            return "Happy";
+    }
+}
